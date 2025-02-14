e53e22a403549f33af8984a190ae08a93a820165
farlos <thiraphat.pant@gmail.com>
2025-02-15 02:16:47 +0700

    Commit with npm

---
+++ src/components/Globe.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Stars, Line } from "@react-three/drei";
+import { TextureLoader } from "three";
+import { useRef, useState, useEffect } from "react";
+
+const Globe = () => {
+  const globeRef = useRef();
+  const [texture, setTexture] = useState(null);
+
+  useEffect(() => {
+    const loadTexture = async () => {
+      const tex = await new TextureLoader().load("/00_earthmap1k.jpg");
+      setTexture(tex);
+    };
+    loadTexture();
+  }, []);
+
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <Stars radius={100} depth={50} count={400} factor={4} />
+
+      {texture && (
+        <mesh ref={globeRef} rotation={[0.4, 0, 0.2]}>
+          <sphereGeometry args={[1, 64, 64]} />
+          <meshBasicMaterial map={texture} />
+        </mesh>
+      )}
+
+      <OrbitControls
+        enableZoom={true}
+        minDistance={2} 
+        maxDistance={10} 
+      />
+    </Canvas>
+  );
+};
+
+export default Globe;
